59749680289d53246f3c9219f943456201573414
leo <leo.liang@jobfutures.com.au>
2011-02-24 19:38:29 +1100

    refactoring proxy_granting_ticket

--- lib/cassify/models/proxy_granting_ticket.rb
+++ lib/cassify/models/proxy_granting_ticket.rb
@@ -1,57 +1,52 @@
 module Cassify
   module Models
     class ProxyGrantingTicket < Ticket
+      @url = nil
+      attr_accessor :url
       set_table_name 'casserver_pgt'
+      
       belongs_to :service_ticket
       has_many :granted_proxy_tickets,
         :class_name => 'ProxyTicket',
         :foreign_key => :granted_by_pgt_id
       
-      # TODO: Need refactoring
-      # Move https validate part to a new validation method
-      # Create a new attribute for pgt_url
-      def self.generate!(pgt_url, host_name, st)
-
-        uri = URI.parse(pgt_url)
+      validate :callback_uri_should_be_valid
+      after_save :log_ticket
+      
+      def callback_uri_should_be_valid
+        uri = URI.parse(url)
         path = uri.path.empty? ? '/' : uri.path
         path += '?' + uri.query unless (uri.query.nil? || uri.query.empty?)
+        path += (uri.query.nil? || uri.query.empty? ? '?' : '&') + "pgtId=#{ticket}&pgtIou=#{iou}"
         
         https = Net::HTTP.new(uri.host,uri.port)
-        https.use_ssl = true
-
-        # Here's what's going on here:
-        #
-        #   1. We generate a ProxyGrantingTicket (but don't store it in the database just yet)
-        #   2. Deposit the PGT and it's associated IOU at the proxy callback URL.
-        #   3. If the proxy callback URL responds with HTTP code 200, store the PGT and return it;
-        #      otherwise don't save it and return nothing.
-        #
+        https.use_ssl = true      
+        begin
+          https.start do |conn|
+            response = conn.request_get(path)
+            unless %w(200 202 301 302 304).include?(response.code)
+              errors.add(:url, "PGT callback server responded with a bad result code '#{response.code}'")
+            end
+          end
+        rescue Errno::ECONNREFUSED
+          errors.add(:pgt_url, "#{url} do not responded")
+        end
+      end
+      
+      def log_ticket
+        logger.debug "PGT generated for pgt_url '#{pgt_url}': #{inspect}"
+      end
+      
+      def self.generate!(pgt_url, host_name, st)        
         pgt = ProxyGrantingTicket.new(
           :ticket             => "PGT-" + Cassify::Utils.random_string(60),
           :iou                => "PGTIOU-" + Cassify::Utils.random_string(57),
           :service_ticket_id  => st.id,
           :client_hostname    => host_name
         )
-
-        # FIXME: The CAS protocol spec says to use 'pgt' as the parameter, but in practice
-        #         the JA-SIG and Yale server implementations use pgtId. We'll go with the
-        #         in-practice standard.
-        path += (uri.query.nil? || uri.query.empty? ? '?' : '&') + "pgtId=#{pgt.ticket}&pgtIou=#{pgt.iou}"
-              
-        https.start do |conn|
-          response = conn.request_get(path)
-          # TODO: follow redirects... 2.5.4 says that redirects MAY be followed
-          # NOTE: The following response codes are valid according to the JA-SIG implementation even without following redirects
-
-          if %w(200 202 301 302 304).include?(response.code)
-            # 3.4 (proxy-granting ticket IOU)
-            pgt.save!
-            logger.debug "PGT generated for pgt_url '#{pgt_url}': #{pgt.inspect}"
-            pgt
-          else
-            logger.warn "PGT callback server responded with a bad result code '#{response.code}'. PGT will not be stored."
-            nil
-          end
+        pgt.url = pgt_url
+        if pgt.save!
+          pgt
         end
       end
       
--- spec/models/proxy_granting_ticket_spec.rb
+++ spec/models/proxy_granting_ticket_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe Cassify::Models::ProxyGrantingTicket do
+  
   it "should have to_s method" do
-    Cassify::Models::ProxyGrantingTicket.generate!("http://localhost:3000").to_s.should match /^LT-/
+    @ticket_granting_ticket = Cassify::Models::TicketGrantingTicket.generate!("user@user.com", "localhost", :roles => ['user', 'superuser'])
+    @service_ticket = Cassify::Models::ServiceTicket.generate!("http://localhost:3000", "user@user.com", "localhost", @ticket_granting_ticket)
+    @proxy_granted_ticket = Cassify::Models::ProxyGrantingTicket.generate!("http://localhost:3000", "http://localhost:3000", @service_ticket)
+    @proxy_granted_ticket.should_not be_nil
+    @proxy_granted_ticket.to_s.should match /^LT-/
   end
 end
