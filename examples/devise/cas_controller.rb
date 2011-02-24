7751e066b028f3e41e79ca4eecb2f7aaeeebce98
leo <leo.liang@jobfutures.com.au>
2011-02-24 18:20:12 +1100

    fix a small bug

--- examples/devise/cas_controller.rb
+++ examples/devise/cas_controller.rb
@@ -4,16 +4,22 @@ class CasController < ApplicationController
   def grant
     begin
       ticket_granting_ticket = Cassify::Models::TicketGrantingTicket.validate(cookies[:tgt])
-      service_ticket         = Cassify::Models::ServiceTicket.generate!(ticket_granting_ticket.username, service, host_name, ticket_granting_ticket)
+      service_ticket         = Cassify::Models::ServiceTicket.generate!(service, ticket_granting_ticket.username, host_name, ticket_granting_ticket)
       redirect_to Cassify::Utils.service_uri_with_ticket(service, service_ticket.to_s)
-    rescue
+    rescue Cassify::Error => e
+      flash[:error] = e.message
       redirect_to new_user_session_path(:service => service)
     end
   end
 
   def validate
     @validator = Cassify::ServiceValidate.new(params[:service], params[:ticket], params[:service] || request.referrer).validate
-    render :template => 'service_validate.builder', :content_type => :xml
+    if @validator.success
+      render :template => 'service_validate.builder', :content_type => :xml
+    else
+      render :template => 'validate_error.builder', :content_type => :xml
+    end
+    
   end
   
   def host_name
